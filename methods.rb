1c3675d8a36551c47982211cd7fc8734b4df45cc
Anthony Hill <hillganthony@outlook.com>
2021-11-01 17:23:27 -0400

    commit tree 0287ad14a3c4d70d9a202470ba960077855a8ae2

--- methods.rb
+++ methods.rb
@@ -1,1 +1,38 @@
-# Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+greet("Naureen")
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}! "
+end
+
+greet_with_default()
+greet_with_default("Sunny")
+
+def add(num1,num2)
+    num1 + num2
+end
+
+add(1,2)
+
+def halve(number)
+ if number.class != number
+ return nil
+ else
+ number.class == number.to_i
+ number / 2
+ end
+end
+
+
+
+
+
+
